e033c3474d1d71da91e3c0a821416fa4971ebee4
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-08-15 22:22:14 +0530

    [Gayatri: added travel package repository]

---
+++ src/main/java/com/bridgelabz/repository/TravelPackageRepository.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.repository;
+
+import com.bridgelabz.model.TravelPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TravelPackageRepository
+        extends JpaRepository<TravelPackage, Long> {
+
+    List<TravelPackage> findByDestination_Id(Long destinationId);
+}
