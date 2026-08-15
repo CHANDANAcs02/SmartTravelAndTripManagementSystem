ee9530eed02e26afc92561e4e7f55e2216ac4032
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-08-15 22:24:08 +0530

    [Gayatri: added destination not found exception]

---
+++ src/main/java/com/bridgelabz/exception/DestinationNotFoundException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.exception;
+
+public class DestinationNotFoundException extends RuntimeException {
+
+    public DestinationNotFoundException(String message) {
+        super(message);
+    }
+}
