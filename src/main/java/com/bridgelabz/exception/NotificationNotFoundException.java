e53f872bc45c29a74b9990e39960d0ceaffce1a9
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-08-16 21:53:52 +0530

    [Ariyan Pujari] created notification not found exception

---
+++ src/main/java/com/bridgelabz/exception/NotificationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.exception;
+
+public class NotificationNotFoundException extends Exception{
+    public NotificationNotFoundException(String message){
+        super(message);
+    }
+}
